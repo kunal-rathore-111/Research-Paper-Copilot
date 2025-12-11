b09fe10813fb1a929b60c7694dd4ac697e7490e9
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-12-11 16:17:00 +0530

    fixed the models calling with github provided models

--- mayank/minor/b/src/utils/catchAsync.js
+++
@@ -1,8 +0,0 @@
-
-function asyncWrapper(fn) {
-    return (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch(next); // it tries the route if having any error it will call the global error middleware via catch
-    }
-}
-
-module.exports = asyncWrapper;
